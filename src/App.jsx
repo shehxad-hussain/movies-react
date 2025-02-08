d930c30adb002863016a8078dd2aeff5e919d65f
m-shehzad455 <shehzuali5@gmail.com>
2025-02-08 09:31:46 +0500

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+const Card = ({title}) => {
+  const [hasLiked,setHasLiked] = useState(false);
+
+
+  return(
+    <div className="card">
+      <h2> 
+        {title}
+        
+      </h2>
+       <button onClick={() => {setHasLiked(!hasLiked)}}>
+        {hasLiked ? 'Liked' :'Like'}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
+
+
+
+
+
+
+const App = () => {
+
+
+  return(
+    <div className="card-container">
+
+      <h2>Functional Arrow Components</h2>
+        <Card   title= "Star Wars"/>
+        <Card  title= "Avatar"/>
+        <Card  title = "The Lion King"/>
+
+    </div>
+  )
+
+}
+
+export default  App;
