dd5658132d2bc54ee82c76c6c635a9bd5c2a123c
m-shehzad455 <shehzuali5@gmail.com>
2025-02-26 23:46:57 +0500

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
+
 
 const App = () => {
+  const [searchTerm, setsearchTerm]  = useState('');
+
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className='text-gradient'>Movies</span>You 'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search  searchTerm= {searchTerm}  setsearchTerm= {setsearchTerm}/> 
+        <h1 className='text-white'>{searchTerm}</h1>
+      </div>
+   </main>
   )
 }
 
